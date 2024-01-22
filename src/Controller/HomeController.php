8299cec2d4b09bf6ad71306beabad1fe55d78b5f
Mehdi Key <formk@kermed.fr>
2024-01-22 16:27:45 +0100

    Ajout de la pagination dans le template

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,20 +3,36 @@
 namespace App\Controller;
 
 use App\Repository\FormationRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(FormationRepository $formationRepository): Response
+    public function index(
+        FormationRepository $formationRepository, 
+        PaginatorInterface $paginator, 
+        Request $request): Response
     {
-        $formations = $formationRepository->findAll();
-        
+
+        $formations = $paginator->paginate(
+            $formationRepository->findBy([],['id' => 'DESC']),
+            $request->query->getInt('page',1),
+            10
+        );
+
         return $this->render('home/index.html.twig', [
             'formations' => $formations,
         ]);
+
+        // $formations = $formationRepository->findAll();
+        
+        // return $this->render('home/index.html.twig', [
+        //     'formations' => $formations,
+        // ]);
    
         // return $this->render('home/index.html.twig', [
         //     'formations' => $formationRepository->findAll(),
