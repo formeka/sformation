17e3c06838becce7b63aa07b8e9fde21bdab03a9
Mehdi Key <formk@kermed.fr>
2024-01-22 14:55:30 +0100

    Ajout contrainte sur champ formulaire

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,21 +5,51 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Range;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('nom', TextType::class,[
+                'attr' => ['class' => 'nom']
+            ])
+            ->add('prenom', TextType::class)
+            // ->add('age', IntegerType::class,[
+            //     'attr' => [
+            //         'min' => 18,
+            //         'max' => 99
+            //     ]
+            // ])
+
+            ->add('age', IntegerType::class,[
+                'constraints' => [
+                    new Range(
+                        [
+                            'min' => 18,
+                            'max' => 100,
+                            'notInRangeMessage' => 'Votre âge doit être compris entre {{ min }} et {{ max }} ans !'
+                        ]
+                    )
+                ]
+
+            ])
+
+            ->add('telephone', TextType::class)
+            ->add('adresse', TextType::class)
+            ->add('email', EmailType::class)
             ->add('agreeTerms', CheckboxType::class, [
-                                'mapped' => false,
+                'mapped' => false,
                 'constraints' => [
                     new IsTrue([
                         'message' => 'You should agree to our terms.',
@@ -27,7 +57,7 @@ class RegistrationFormType extends AbstractType
                 ],
             ])
             ->add('plainPassword', PasswordType::class, [
-                                // instead of being set onto the object directly,
+                // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
@@ -42,8 +72,7 @@ class RegistrationFormType extends AbstractType
                         'max' => 4096,
                     ]),
                 ],
-            ])
-        ;
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
