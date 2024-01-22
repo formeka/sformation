515afb198df6cf81e85a8683821702cbd0bcc93e
Mehdi Key <formk@kermed.fr>
2024-01-22 11:39:32 +0100

    Mise à jour fixtures user

---
+++ migrations/Version20240122101425.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240122101425 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE formation (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, titre VARCHAR(255) NOT NULL, description CLOB NOT NULL, duree INTEGER NOT NULL, niveau VARCHAR(20) NOT NULL, lieu VARCHAR(15) NOT NULL, resume CLOB NOT NULL)');
+        $this->addSql('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles CLOB NOT NULL --(DC2Type:json)
+        , password VARCHAR(255) NOT NULL, nom VARCHAR(255) NOT NULL, prenom VARCHAR(255) NOT NULL, age INTEGER NOT NULL, telephone INTEGER NOT NULL, adresse VARCHAR(255) NOT NULL)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON user (email)');
+        $this->addSql('CREATE TABLE messenger_messages (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, body CLOB NOT NULL, headers CLOB NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL --(DC2Type:datetime_immutable)
+        , available_at DATETIME NOT NULL --(DC2Type:datetime_immutable)
+        , delivered_at DATETIME DEFAULT NULL --(DC2Type:datetime_immutable)
+        )');
+        $this->addSql('CREATE INDEX IDX_75EA56E0FB7336F0 ON messenger_messages (queue_name)');
+        $this->addSql('CREATE INDEX IDX_75EA56E0E3BD61CE ON messenger_messages (available_at)');
+        $this->addSql('CREATE INDEX IDX_75EA56E016BA31DB ON messenger_messages (delivered_at)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE formation');
+        $this->addSql('DROP TABLE user');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -12,15 +12,15 @@ class UserFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('fr_FR');
-        for($i=0; $i<=5; $i++):
+        for ($i = 0; $i <= 5; $i++) :
             $user = new User();
             $user->setNom($faker->lastName());
             $user->setPrenom($faker->firstName());
-            $user->setAge($faker->numberBetween(18,99));
-            $user->setTelephone($faker->e164PhoneNumber());
+            $user->setAge($faker->numberBetween(18, 99));
+            $user->setTelephone($faker->randomElement(['0235569852', '0335569852', '0435569852','0535569852']));
             $user->setEmail($faker->email());
             $user->setAdresse($faker->address());
-            $user->setRoles($faker->randomElement([['ROLE_USER'],['ROLE_ADMIN']]));
+            $user->setRoles($faker->randomElement([['ROLE_USER'], ['ROLE_ADMIN']]));
             $user->setPassword($faker->password());
 
 
