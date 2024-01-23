052f36492d1e10e0c0bec14496fb670520dab31a
Mehdi Key <formk@kermed.fr>
2024-01-23 15:03:46 +0100

    Ajour crud user

---
+++ src/Form/FormationType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Formation;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FormationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre')
+            ->add('description')
+            ->add('duree')
+            ->add('niveau')
+            ->add('lieu')
+            ->add('resume')
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+'choice_label' => 'titre',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Formation::class,
+        ]);
+    }
+}
